f9c1847b348220aaffc78bc5249169e7f764c6b6
Tommaso Sardelli <lacapannadelloziotom@gmail.com>
2018-12-28 19:07:14 +0100

    Add a python script to send messages in multicast

---
+++ utils/multicast_publisher.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+
+import argparse
+import socket
+
+parser = argparse.ArgumentParser()
+parser.add_argument("message")
+args = parser.parse_args()
+
+message_bytes = args.message.encode()
+multicast_group = ('230.0.0.0', 5001)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sent = sock.sendto(message_bytes, multicast_group)
+print("done sending: {}".format(args.message))
